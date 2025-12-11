2f313263badb1effd5d6479424c5a98256295d53
Aidan-Kelley <aidank6@yahoo.com>
2025-12-11 04:24:22 -0800

    table with million trials

---
+++ gammonutil.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include "random.c"
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+    char size;
+    char tiles[6];
+} Board;
+
+static inline bool makeMove(Board* board, int die) {
+    char* tiles = board->tiles;
+    char size = board->size;
+    if (die >= size) {
+        tiles[size - 1]--;
+    } else if (tiles[die] != 0) {
+        tiles[die]--;
+    } else {
+        tiles[size - 1]--;
+        tiles[size - 1 - (die + 1)]++;
+    }
+    while (tiles[board->size - 1] == 0) {
+        board->size--;
+        if(board->size == 0)
+            return true;
+    }
+    return false;
+}
+
+int* roll() {
+    int die1 = randInt6();
+    int die2 = randInt6();
+    static int arr[2];
+    if (die1 > die2) {
+        arr[0] = die1;
+        arr[1] = die2;
+    } else {
+        arr[0] = die2;
+        arr[1] = die1;
+    }
+    return arr;
+}
+
+void print(const Board board, const int* roll, int p) {
+    printf("Player %d Roll: %d, %d\n", p, roll[0], roll[1]);
+    printf("Player %d Board: ", p);
+    printf("%d",board.tiles[0]);
+    for (int i = 1; i < board.size; i++) {
+        printf(", %d",board.tiles[i]);
+    }
+    printf("\n");
+}
+
+size_t determineSize(const char* board) {
+    for (int i = 5; i >= 0; i--) {
+        if (board[i] != 0) {
+            return i + 1;
+        }
+    }
+}
+
+bool player_turn(Board* board) {
+    int* currentRoll = roll(); 
+    if (makeMove(board, currentRoll[0])) return true;
+    if (makeMove(board, currentRoll[1])) return true;
+    if(currentRoll[0] == currentRoll[1]) { // doubles
+        if (makeMove(board, currentRoll[0])) return true;
+        if (makeMove(board, currentRoll[1])) return true;
+    }
+    return false;
+}
+
+uint32_t compressBoard(const char* board) {
+    uint32_t result = 0;
+    for (int i = 0; i < 6; i++) {
+        result |= board[i] << (i * 4);
+    }
+    return result;
+}
--- main.c
+++ main.c
@@ -3,81 +3,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include "gammonutil.c"
 
-typedef struct {
-    char size;
-    char tiles[6];
-} Board;
-
-#define STARTING_BOARD {1,0,2,1,4,6}
+#define PLAYER_ONE_BOARD {1,0,2,1,4,6}
 #define PLAYER_TWO_BOARD {0,3,0,0,3,8}
 
-static inline bool makeMove(Board* board, int die) {
-    char* tiles = board->tiles;
-    char size = board->size;
-    if (die >= size) {
-        tiles[size - 1]--;
-    } else if (tiles[die] != 0) {
-        tiles[die]--;
-    } else {
-        tiles[size - 1]--;
-        tiles[size - 1 - (die + 1)]++;
-    }
-
-    while (tiles[board->size - 1] == 0) {
-        board->size--;
-        if(board->size == 0)
-            return true;
-    }
-    return false;
-}
-
-int* roll() {
-    int die1 = randInt6();
-    int die2 = randInt6();
-    static int arr[2];
-    if (die1 > die2) {
-        arr[0] = die1;
-        arr[1] = die2;
-    } else {
-        arr[0] = die2;
-        arr[1] = die1;
-    }
-    return arr;
-}
-
-void print(const Board board, const int* roll, int p) {
-    printf("Player %d Roll: %d, %d\n", p, roll[0], roll[1]);
-    printf("Player %d Board: ", p);
-    printf("%d",board.tiles[0]);
-    for (int i = 1; i < 6; i++) {
-        printf(", %d",board.tiles[i]);
-    }
-    printf("\n");
-}
-
-void determineSize(Board* board) {
-    for (int i = 5; i >= 0; i--) {
-        if (board->tiles[i] != 0) {
-            board->size = i + 1;
-            return;
-        }
-    }
-}
-
-bool player_turn(Board* board) {
-    int* currentRoll = roll(); 
-    if (makeMove(board, currentRoll[0])) return true;
-    if (makeMove(board, currentRoll[1])) return true;
-    if(currentRoll[0] == currentRoll[1]) { // doubles
-        if (makeMove(board, currentRoll[0])) return true;
-        if (makeMove(board, currentRoll[1])) return true;
-    }
-    return false;
-}
-
 bool runGame() {
-    Board player1Board = {0, STARTING_BOARD};
+    Board player1Board = {0, PLAYER_ONE_BOARD};
     Board player2Board = {0, PLAYER_TWO_BOARD};
 
     determineSize(&player1Board);
@@ -89,7 +21,7 @@ bool runGame() {
     }
 }
 bool runDebugGame() {
-    Board player1Board = {0, STARTING_BOARD};
+    Board player1Board = {0, PLAYER_ONE_BOARD};
     Board player2Board = {0, PLAYER_TWO_BOARD};
 
     determineSize(&player1Board);
